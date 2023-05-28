ddcfcc4cf3d7b87652e891220fa45050740b562d
Jatin Shrivastav <mejrseinc@gmail.com>
2023-05-28 13:22:10 +0530

    Misc: Semicolon Typo Fix In Index.ts

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -16,7 +16,7 @@ createConnection()
 
     // register express routes from defined application routes
     Routes.forEach((route) => {
-      ;(app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+      (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
         const result = new (route.controller as any)()[route.action](req, res, next)
         if (result instanceof Promise) {
           result.then((result) => (result !== null && result !== undefined ? res.send(result) : undefined))
