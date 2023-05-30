95fd76d6cc7d1e26da2202214bb2c57c8407dec3
Jatin Shrivastav <mejrseinc@gmail.com>
2023-05-30 23:21:58 +0530

    FIx: Fixed Student Group DTO

--- back-end/src/dto/student.dto.ts
+++ back-end/src/dto/student.dto.ts
@@ -1,6 +1,5 @@
-export class StudentGroup {
-    id: number;
-    first_name: string;
-    last_name: string;
-    full_name:string;
+export class StudentGroupDTO {
+    student_id: number;
+    group_id: number;
+    incident_count :number;
 }
